491b90c07b1cc1a2a9e90e916bbe573690aa2842
B4bolFP <mario.lopezl@campusfp.es>
2023-05-16 10:21:16 +0200

    s

--- script/apis.js
+++ script/apis.js
@@ -1,15 +1,21 @@
+// Seleccionamos donde vamos a meter las filas de la tabla con querySelector
 const repositorio_tbody = document.querySelector(".repositorio-tbody");
 
+// Citamos el json deseado
 fetch('https://www.el-tiempo.net/api/json/v2/provincias')
 .then(function(response) {
     return response.json();
 })
 
 .then(function(datos) {
+    // Declaramos la variable que va a contener las filas te la tabla
     let out_tbody = '';
+    // ya que "datos" contiene la informacion del json accedemos al apartado de provincias
     x = datos.provincias;
 
+    // Como hemos guardado todas las entradas de "provincias" ahora podemos acceder una a una
     for (let dato of x) {
+        // Cada "dato tiene sus comunas y podemos acceder a estas con dato.{x}"
         out_tbody += `
             <tr>
                 <td>${dato.CODPROV}</td>
@@ -21,5 +27,6 @@ fetch('https://www.el-tiempo.net/api/json/v2/provincias')
         `;
     }
 
+    // por ultimo le pasamos el megaString a el contenedor de la tabla.
     repositorio_tbody.innerHTML = out_tbody;
 })
